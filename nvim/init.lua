f9cd3cce49a949f54fff3a16361a257f4694ec7e
Arti <ideaking1984@proton.me>
2024-12-31 16:59:59 +0200

    initial commit

---
+++ nvim/init.lua
@@ -0,0 +1,18 @@
+-- add line numbers
+vim.opt.number = true
+-- add system clipboard
+vim.opt.clipboard = "unnamedplus"
+
+-- auto create directories and files if does not exists
+vim.api.nvim_create_augroup('AutoCreateDir', { clear = true })
+vim.api.nvim_create_autocmd('BufWritePre', {
+	group = 'AutoCreateDir',
+	pattern = '*',
+	callback = function()
+		local dir = vim.fn.expand('<afile>:p:h')
+		if vim.fn.isdirectory(dir) == 0 then
+			vim.fn.mkdir(dir, 'p')
+		end
+	end,
+})
+
